d3bf96f0b28b7e8bf5ef709fc3a6f6708be4f70a
Callum C <cpcooper34@hotmail.com>
2023-08-10 16:28:43 +1200

    feat: Added scss support

--- audialytix/audialytix/settings.py
+++ audialytix/audialytix/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from dotenv import load_dotenv
+import os
 from pathlib import Path
 
-import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -143,10 +143,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
+STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Compressors
+COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- audialytix/pages/urls.py
+++ audialytix/pages/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import HomePageView, home
+from .views import HomePageView
 
 urlpatterns = [
-    path("", home, name="home")
-    # path("", HomePageView.as_view(), name="home"),
+    path("", HomePageView.as_view(), name="home"),
 ]
