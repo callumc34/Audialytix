e3b55a0a83afd26eeeb1496ee06e1a58b2569cb1
Callum C <cpcooper34@hotmail.com>
2023-08-21 17:12:55 +1200

    fix: Moved models to api

--- audialytix/api/models.py
+++ audialytix/api/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Song(models.Model):
+    STEREO = "stereo"
+    MONO = "mono"
+    AUDIO_CHOICES = [
+        (STEREO, "Stereo"),
+        (MONO, "Mono"),
+    ]
+
+    artist = models.CharField(max_length=255)
+    song_name = models.CharField(max_length=255)
+    audio_type = models.CharField(max_length=10, choices=AUDIO_CHOICES)
+    audio_points = models.JSONField()  # Assuming this stores an array of doubles
+
+    onset_analysis = models.JSONField(
+        blank=True, null=True
+    )  # Optional array of doubles
+    spectral_analysis = models.JSONField(
+        blank=True, null=True
+    )  # Optional array of doubles
+
+    def __str__(self):
+        return f"{self.artist} - {self.song_name}"
